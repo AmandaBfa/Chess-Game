ecded109764be9a5c2f7df89e596f29f8188cf89
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2026-01-13 15:44:53 -0300

    creating the rules of chess pieces

--- app/Services/Chess/Pieces/King.php
+++ app/Services/Chess/Pieces/King.php
@@ -6,10 +6,16 @@ class King extends Piece
 {
     public function canMove(array $board, int $fromRow, int $fromCol, int $toRow, int $toCol): bool
     {
+        // evita movimentos nulos
+        if ($fromRow === $toRow && $fromCol === $toCol) {
+            return false;
+        }
+
         // vai calcular a diferença absoluta entre as linhas e colunas
         $rowDiff = abs($fromRow - $toRow);
         $colDiff = abs($fromCol - $toCol);
 
+        // validação do movimento do rei, que pode andar apenas uma casa em qualquer direção
         if (!($rowDiff <= 1 && $colDiff <= 1)) {
             return false;
         }
